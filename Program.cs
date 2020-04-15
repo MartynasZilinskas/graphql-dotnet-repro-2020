37e689df9e7fb7b25efcc9394f64c305e0843141
Martynas Zilinskas <martynas@quatrodev.com>
2020-04-16 01:46:38 +0300

    Added files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using GraphQL.Utilities.Federation;
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schemaPath = "schema.graphql";
+            var schema = File.ReadAllText(schemaPath);
+
+            var federatedSchema = FederatedSchema.For(schema, _ => { });
+            var printer = new FederatedSchemaPrinter(federatedSchema);
+            Console.WriteLine(printer.PrintFederatedSchema());
+        }
+    }
+}
